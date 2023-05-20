0c3f6c681a4332152e73a20061447fe30ec42a01
Akhilvalluru1 <109025277+AkhilV14@users.noreply.github.com>
2023-05-20 12:12:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import { Alert } from 'reactstrap';
 import './App.css';
-
+import { Badge, Button } from 'reactstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+    <Alert color="danger">
+    Hello !! Akhil Kumar
+      </Alert>
+      <Button color="primary" outline>
+          Notifications <Badge color="secondary">4</Badge>
+        </Button></div>
   );
 }
 
